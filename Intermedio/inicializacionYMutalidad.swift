5c601830f44c29c457c41b2d6c6a61e941f65091
AZ-desarrollador4 <desarrollador4@azsmart.net>
2023-02-12 10:26:12 -0500

    inicializacionYmutalidad

---
+++ Intermedio/inicializacionYMutalidad.swift
@@ -0,0 +1,13 @@
+var emptyString = ""
+var anotherString = String()
+
+if emptyString.isEmpty{
+    print("Nada que ver aquí")
+    
+}else {
+    print("El string tiene valor")
+    
+}
+
+
+print(emptyString)
